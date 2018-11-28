5d40fbfdf56bdb17dc2a036b179838f69c1e2db6
root <ryder1986@163.com>
2018-11-28 11:36:39 +0800

    1

--- test.h
+++ test.h
@@ -32,6 +32,96 @@ public:
         ENCODE_INT_MEM(y);
     }
 };
+class DummyProcessor_Output:public JsonData{
+
+public:
+    vector<VdPoint> Points;
+    int Radii;
+    DummyProcessor_Output()
+    {
+    }
+    DummyProcessor_Output(JsonPacket str):JsonData(str)
+    {
+        decode();
+    }
+    DummyProcessor_Output(vector<VdPoint> o,int r):Radii(r)
+    {
+        Points.assign(o.begin(),o.end());
+        encode();
+    }
+    void decode()
+    {
+        try{
+            DECODE_JSONDATA_ARRAY_MEM(Points);
+            DECODE_INT_MEM(Radii);
+        }catch(exception e){
+            PRT_DECODE_EXCEPTION
+        }
+    }
+    void encode()
+    {
+        try{
+            ENCODE_JSONDATA_ARRAY_MEM(Points);
+            ENCODE_INT_MEM(Radii);
+        }catch(exception e){
+            PRT_ENCODE_EXCEPTION
+        }
+    }
+    template <typename A,typename B,typename C>
+    void draw(int offx,int offy,
+              A draw_line,
+              B draw_circle,C draw_text)
+    {
+        for(VdPoint p:Points){
+            draw_circle(VdPoint(p.x+offx,p.y+offy),Radii,PaintableData::Colour::Red,2);
+        }
+    }
+
+};
+class DummyProcessor_Pri:public JsonData,public PaintableData{
+
+public:
+    bool Horizon;
+    bool Vertical;
+    int Radii;
+    DummyProcessor_Pri(bool h,bool v,int r):Horizon(h),Vertical(v),Radii(r)
+    {
+        encode();
+    }
+    DummyProcessor_Pri(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    DummyProcessor_Pri()
+    {
+
+    }
+    void decode()
+    {
+        try{
+            DECODE_BOOL_MEM(Horizon);
+            DECODE_BOOL_MEM(Vertical);
+            DECODE_INT_MEM(Radii);
+        }catch(exception e){
+            PRT_DECODE_EXCEPTION
+        }
+    }
+    void encode()
+    {
+        try{
+            ENCODE_BOOL_MEM(Horizon);
+            ENCODE_BOOL_MEM(Vertical);
+            ENCODE_INT_MEM(Radii);
+        }catch(exception e){
+            PRT_DECODE_EXCEPTION
+        }
+    }
+    static DummyProcessor_Pri get_dummy_test_data()
+    {
+        DummyProcessor_Pri d(true,false,17);
+        return d;
+    }
+};
 
 class DetectRegion_Pri:public JsonData{
 public:
@@ -136,8 +226,69 @@ public:
 
 
 
+
+
 #include "videosource.h"
 
+class VideoProcessor
+{
+public:
+    string alg_rst;
+
+    VideoProcessor()
+    {
+
+    }
+
+    virtual ~VideoProcessor()
+    {
+
+    }
+
+    virtual bool modify_processor(JsonPacket p)
+    {
+        return true;
+    }
+
+    virtual bool process(Mat img_src,JsonPacket &pkt)=0;
+
+    virtual  string get_rst()
+    {
+        string ba;
+        return ba ;
+    }
+
+    virtual void init()
+    {
+    }
+
+protected:
+private:
+
+};
+
+
+class Region_Manager:public VdData<DetectRegion_Pri>
+{
+public:
+    enum OP{
+        CHANGE_RECT=1,
+        CHANGE_PROCESSOR,
+        MODIFY_PROCESSOR
+    };
+    Region_Manager(DetectRegion_Pri pkt):VdData(pkt)
+    {
+
+    }
+    ~Region_Manager()
+    {
+
+    }
+
+private:
+
+
+};
 class Camera_Manager:public VdData<CameraData_Pri>
 {
 public:
@@ -152,12 +303,10 @@ public:
     }
     ~Camera_Manager()
     {
-        prt(info,"destr");
         delete p_src;
     }
     Camera_Manager(Camera_Manager&&r)
     {
-        prt(info,"start camera right");
         p_src=r.p_src;
         r.p_src=nullptr;
     }
@@ -165,7 +314,6 @@ public:
     Camera_Manager(const Camera_Manager&m)
     {
         p_src=m.p_src;
-        prt(info,"start camera copy");
     }
 private:
 
@@ -173,7 +321,6 @@ private:
     VideoSource *p_src;
 
 };
-
 
 class Cameras_Manager:public VdData<CameraManagerData_Pri>
 {
@@ -185,9 +332,7 @@ public:
         VdData(pkt)
     {
         for(CameraData_Pri &cam_config:pkt.CameraData){
-           Camera_Manager m(cam_config);
-           Camera_Manager &mm=m;
-           cameras.push_back(mm);
+           cameras.push_back(Camera_Manager(cam_config));
         }
     }
 private:
